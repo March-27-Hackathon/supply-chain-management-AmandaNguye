c5a516d04b65822aeff3f5d977492f692e1950a4
Tommyt19 <tommy.ntn.tran@gmail.com>
2021-03-27 11:51:32 -0600

    Updated Chair.java
    changed private data members to booleans from chars

--- edu/ucalgary/ensf409/Chair.java
+++ edu/ucalgary/ensf409/Chair.java
@@ -7,37 +7,37 @@ package edu.ucalgary.ensf409;
 
 public class Chair extends Furniture {
     // class fields
-    private char legs;
-    private char arms;
-    private char seat;
-    private char cushion;
+    private boolean legs;
+    private boolean arms;
+    private boolean seat;
+    private boolean cushion;
     // class methods
 
     // getter methods
-    public char getArms() {
+    public boolean getArms() {
         return arms;
     }
-    public char getCushion() {
+    public boolean getCushion() {
         return cushion;
     }
-    public char getLegs() {
+    public boolean getLegs() {
         return legs;
     }
-    public char getSeat() {
+    public boolean getSeat() {
         return seat;
     }
 
     // setter methods
-    public void setArms(char arms) {
+    public void setArms(boolean arms) {
         this.arms = arms;
     }
-    public void setCushion(char cushion) {
+    public void setCushion(boolean cushion) {
         this.cushion = cushion;
     }
-    public void setLegs(char legs) {
+    public void setLegs(boolean legs) {
         this.legs = legs;
     }
-    public void setSeat(char seat) {
+    public void setSeat(boolean seat) {
         this.seat = seat;
     }
 
