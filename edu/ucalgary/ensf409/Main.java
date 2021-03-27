da9b56993896c16f814b0d3c084ef64b75dd0926
AmandaNguye <77892730+AmandaNguye@users.noreply.github.com>
2021-03-27 11:52:47 -0600

    Update Main.java

--- edu/ucalgary/ensf409/Main.java
+++ edu/ucalgary/ensf409/Main.java
@@ -1,8 +1,21 @@
+/**
+ * @author Amanda <a href="mailto:amanda.nguyen1@ucalgary.ca"> amanda.nguyen1@ucalgary.ca</a>
+ * @version 1.0
+ * @since 1.0
+*/
 package edu.ucalgary.ensf409;
 
-public class Main
-{
+import java.io.*;
+
+public class Main {
     public static void main(String[] args) {
-        //Main Instructions
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter your Funiture Category:");
+        String category = reader.readLine();
+        System.out.println("Enter your furniture type:");
+        String type = reader.readLine();
+        System.out.println("Enter your furniture count:");
+        String count = reader.readLine();
+
     }
 }
