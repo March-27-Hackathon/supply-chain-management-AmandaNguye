a2662a60de1844e59dd4bc6c4213098f38adcec6
datlam25 <d4nnyl4m@gmail.com>
2021-03-27 11:26:27 -0600

    Change Hello to Main

--- edu/ucalgary/ensf409/Main.java
+++ edu/ucalgary/ensf409/Main.java
@@ -1,8 +1,8 @@
 package edu.ucalgary.ensf409;
 
-public class hello
+public class Main
 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        //Main Instructions
     }
 }
