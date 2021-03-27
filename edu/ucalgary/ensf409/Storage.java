d848d25cc370c973b4d6a5c2e95efea065aca88d
Tyler Tran <haxkue@gmail.com>
2021-03-27 14:21:09 -0600

    made class concrete
    in order for better implementation of storage

--- edu/ucalgary/ensf409/Storage.java
+++ edu/ucalgary/ensf409/Storage.java
@@ -11,7 +11,8 @@ public class Storage {
     private Connection dbConnect;
     private ResultSet result;
 
-    HashMap<String, Furniture> chairStorage;
+    private HashMap<String, Furniture> chairStorage, deskStorage, filingStorage, lampStorage;
+    private HashMap<String, Manufacturer> manufacturerStorage;
 
     public Storage(String dbUrl, String username, String password)
     {
@@ -29,6 +30,10 @@ public class Storage {
         }
 
         chairStorage = populateFurniture("chair");
+        deskStorage = populateFurniture("desk");
+        filingStorage = populateFurniture("filing");
+        lampStorage = populateFurniture("lamp");
+
     }
 
     public void initialize() throws SQLException
@@ -42,11 +47,45 @@ public class Storage {
         try
         {
             result = dbConnect.createStatement().executeQuery("SELECT * FROM "+tableName);
-
             while(result.next())
             {
-                //TODO ADD FULL CHAIR CONSTRUCTOR IMPLEMEMNTATION
-                tempHashSet.put(result.getString("ID"),new Chair());
+                Furniture obj = new Furniture(result.getString("ID"),result.getString("Type"),result.getString("ManuID"),result.getInt("Price"));
+                if(tableName == "chair")
+                {
+                    Chair chair = (Chair)obj;
+                    chair.setLegs(getBool(result.getString("Legs")));
+                    chair.setArms(getBool(result.getString("Arms")));
+                    chair.setSeat(getBool(result.getString("Seat")));
+                    chair.setCushion(getBool(result.getString("Cushion")));
+                    tempHashSet.put(result.getString("ID"),chair);
+                }
+                else if(tableName == "desk")
+                {
+                    Desk desk = (Desk)obj;
+                    desk.setLegs(getBool(result.getString("Legs")));
+                    desk.setTop(getBool(result.getString("Top")));
+                    desk.setDrawer(getBool(result.getString("Drawer")));
+                    tempHashSet.put(result.getString("ID"),desk);
+                }
+                else if(tableName == "filing")
+                {
+                    Filing filing = (Filing)obj;
+                    filing.setCabinet(getBool(result.getString("Cabinet")));
+                    filing.setDrawers(getBool(result.getString("Drawers")));
+                    filing.setRails(getBool(result.getString("Rails")));
+                    tempHashSet.put(result.getString("ID"),filing);
+                }
+                else if(tableName == "lamp")
+                {
+                    Lamp lamp = (Lamp)obj;
+                    lamp.setBase(getBool(result.getString("Base")));
+                    lamp.setBulb(getBool(result.getString("Bulb")));
+                    tempHashSet.put(result.getString("ID"), lamp);
+                }
+                else 
+                {
+                    throw new SQLException();
+                }
             }
         }
         catch(SQLException e)
@@ -56,5 +95,51 @@ public class Storage {
         }
 
         return tempHashSet;
+    }
+
+    private boolean getBool(String c)
+    {
+        return c=="Y";
+    }
+
+    private void removeFromDatabase(String tableName, String key)
+    {
+        try
+        {
+            dbConnect.createStatement().executeQuery("DELETE FROM "+tableName+" WHERE ID = "+key);
+        }
+        catch(SQLException e)
+        {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    }
+
+    //--GETTER METHODS--
+    public HashMap<String, Furniture> getChairStorage() {
+        return chairStorage;
+    }
+
+    public HashMap<String, Furniture> getDeskStorage() {
+        return deskStorage;
+    }
+
+    public HashMap<String, Furniture> getFilingStorage() {
+        return filingStorage;
+    }
+
+    public HashMap<String, Furniture> getLampStorage() {
+        return lampStorage;
+    }
+
+    public HashMap<String, Manufacturer> getManufacturerStorage() {
+        return manufacturerStorage;
+    }
+
+    //--MUTATOR METHODS--
+    public void removeFromStorage(String tableName, String id)
+    {
+        chairStorage.remove(id);
+        removeFromDatabase("chair", id);
     }
 }
