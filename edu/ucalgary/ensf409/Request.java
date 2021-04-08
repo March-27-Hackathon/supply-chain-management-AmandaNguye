781b7862b61f9a144b32fce5d1f7714edc5df34e
AmandaNguye <77892730+AmandaNguye@users.noreply.github.com>
2021-04-07 23:35:41 -0600

    Document Request.java
    add documentation and changed method name

--- edu/ucalgary/ensf409/Request.java
+++ edu/ucalgary/ensf409/Request.java
@@ -1,8 +1,8 @@
 /**
  * @author Amanda Nguyen <a href="mailto:amanda.nguyen1@ucalgary.ca">amanda.nguyen1@ucalgary.ca</a>
  * @author Dat Lam <a href="mailto:dat.lam1@ucalgary.ca">dat.lam1@ucalgary.ca</a>
- * @version 1.4
- * @since 1.1
+ * @version 2.0
+ * @since 1.4
  * 
  */
 
@@ -11,14 +11,18 @@ package edu.ucalgary.ensf409;
 import java.util.ArrayList;
 
 public class Request {
-
+    // CLASS FIELDS
     private Storage storage;
     private int count;
 
+    /**
+     * Constructor of a Request, takes in the storage, an ArrayList of the inventory.
+     */
     public Request(Storage storage) {
         this.storage = storage;
     }
 
+    // CLASS METHODS
     /**
      * Calculate posible combination of furniture that would make a complete
      * furniture of the type, then return the array list of those furnitures.
@@ -41,31 +45,54 @@ public class Request {
         return null;
     }
 
+    /**
+     * Method that tests all of the possible combinations of a certain type of
+     * chair by calling its recursive function.
+     * 
+     * @param list
+     * @param valids
+     */
     private void findValidChairCombos(ArrayList<Chair> list, ArrayList<ArrayList<Chair>> valids) {
         ArrayList<Chair> staged = new ArrayList<Chair>();
-        findCombos(0, list, staged, valids);
+        testChairCombos(0, list, staged, valids);
     }
 
-    private void findCombos(int index, ArrayList<Chair> list, ArrayList<Chair> staged,
+    /**
+     * Method that tests all of the possible combinations of a certain type of
+     * chair and adds valid combinations to a list.
+     * 
+     * @param index  int of list to be added next
+     * @param list   ArrayList<Chair> raw list of typed chair
+     * @param staged ArrayList<Chair> Chair combination to be tested
+     * @param valids ArrayList<ArrayList<Chair>> list of valid Chair combinations
+     */
+    private void testChairCombos(int index, ArrayList<Chair> list, ArrayList<Chair> staged,
             ArrayList<ArrayList<Chair>> valids) {
         if (index >= list.size()) {
             staged.remove(staged.size() - 1);
             if (staged.size() == 0) {
                 return;
             } else {
-                int temp = list.indexOf(staged.get(staged.size() -1));
+                int temp = list.indexOf(staged.get(staged.size() - 1));
                 staged.remove(staged.size() - 1);
-                findCombos(temp + 1, list, staged, valids);
+                testChairCombos(temp + 1, list, staged, valids);
             }
         } else {
             staged.add(list.get(index));
             if (isValidChairCombo(staged)) {
                 valids.add(new ArrayList<Chair>(staged));
-            } 
-            findCombos(index + 1, list, staged, valids);
+            }
+            testChairCombos(index + 1, list, staged, valids);
         }
     }
 
+    /**
+     * Method that returns true if the input Chair combination fills the input
+     * request.
+     * 
+     * @param staged ArrayList<Chair> Chair combination to be tested
+     * @return boolean of combincation validity
+     */
     private boolean isValidChairCombo(ArrayList<Chair> staged) {
         int[] partsToFill = new int[] { 0, 0, 0, 0 };
         for (int i = 0; i < staged.size(); i++) {
@@ -90,6 +117,13 @@ public class Request {
         return true;
     }
 
+    /**
+     * Method that returns the lowest priced Chair combination in a list of Chair
+     * combinations.
+     * 
+     * @param valids ArrayList<ArrayList<Chair>>
+     * @return ArrayList<Chair> lowest priced Chair combination
+     */
     private ArrayList<Chair> findLowestChairCombo(ArrayList<ArrayList<Chair>> valids) {
         if (valids == null) {
             return null;
@@ -103,9 +137,15 @@ public class Request {
         return temp;
     }
 
-    private int priceOf(ArrayList<Chair> list) {
+    /**
+     * Method that returns the price of a Furniture list
+     * 
+     * @param list ArrayList<? extends Furniture>
+     * @return int total price of list
+     */
+    private int priceOf(ArrayList<? extends Furniture> list) {
         int price = 0;
-        for (Chair item : list) {
+        for (Furniture item : list) {
             price += item.getPrice();
         }
         return price;
